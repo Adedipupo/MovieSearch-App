af5428aebc3bd3de8e7f8c6f03ca5e6371d1298e
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-10 02:54:45 +0100

    form

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -5,6 +5,7 @@ import React from 'react';
         <form className="form">
             <label htmlFor="query" className="Label">
                 <input type="text" name="query" placeholder="Search here ..." />
+                <button type="submit" className="button"></button>
             </label>
         </form>
     )
