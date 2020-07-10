dd47e494fe9c304bc7d129127113a0111b7e3373
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-10 02:52:19 +0100

    form

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -3,7 +3,9 @@ import React from 'react';
  function SearchMovies() {
     return(
         <form className="form">
-            
+            <label htmlFor="query" className="Label">
+
+            </label>
         </form>
     )
 }
