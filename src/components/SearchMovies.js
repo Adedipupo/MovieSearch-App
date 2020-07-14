0ecfa22d479aeb90972be4910def12ffe5e5f005
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-14 02:25:32 +0100

    searchFunction

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default searchMovies() {
      const searchMovies = async () => {
-         
+         console.log("submitting!!!!1")
      }
     return(
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
                 placeholder="i.e. Jurassic Park" />
