50557fb5964db654791e28ed007ab19d87982850
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-14 02:40:00 +0100

    api key n

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -5,6 +5,11 @@ function SearchMovies() {
      const searchMovies = async (e) => {
           e.preventDefault();
          console.log("submitting!!!!1");
+
+         const url = `https://api.themoviedb.org/3/search/movie?api_
+                    key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}
+                    &page=1&include_adult=false`;
+
      }
 
     return(
