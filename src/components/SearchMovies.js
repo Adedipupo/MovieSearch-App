3c0dd22fc48d2183e5e2144e285a2bf4a950ebf5
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-14 02:51:20 +0100

    testing query

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,28 +1,34 @@
-import React from 'react';
+import React from "react";
 
 function SearchMovies() {
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("submitting!!!!1");
 
-     const searchMovies = async (e) => {
-          e.preventDefault();
-         console.log("submitting!!!!1");
+    const query = "Jurassic park";
 
-         const query = "Jurassic park"; 
+    const url = `https://api.themoviedb.org/3/movie/550?api_key=fa2fa335b93284c5277907e139256806ab&language=en-US&query=${query}&page=1&include_adult=false`;
 
-         const url = `https://api.themoviedb.org/3/movie/550?api_key=fa2fa335b93284c5277907e139256806ab&language=en-US&query=${query}&page=1&include_adult=false`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  };
 
-           const res = await fetch(url);
-           const data = await res.json();
-           console.log(data);
-
-     }
-
-    return(
-        <form className="form" onSubmit={searchMovies}>
-            <label className="label" htmlFor="query">Movie Name</label>
-            <input className="input" type="text" name="query"
-                placeholder="i.e. Jurassic Park" />
-            <button className="button" type="submit">Search</button>
-        </form>
-    )
+  return (
+    <form className="form" onSubmit={searchMovies}>
+      <label className="label" htmlFor="query">
+        Movie Name
+      </label>
+      <input
+        className="input"
+        type="text"
+        name="query"
+        placeholder="i.e. Jurassic Park"
+      />
+      <button className="button" type="submit">
+        Search
+      </button>
+    </form>
+  );
 }
 export default SearchMovies;
