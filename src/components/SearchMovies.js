ca0d8bad24076f3110365fb21d6b518f4081987d
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-15 01:31:16 +0100

    ading state

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -10,7 +10,7 @@ function SearchMovies() {
   const searchMovies = async (e) => {
     e.preventDefault();
 
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+    const url = `https://api.themoviedb.org/3/movie/550?api_key=fa2fa335b93284c5277907e139256806&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
       const res = await fetch(url);
