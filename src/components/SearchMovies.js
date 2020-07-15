34583072a0514df4a614f63b7d4064a63f108a0e
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-15 01:30:28 +0100

    ading state

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -2,45 +2,39 @@ import React,{useState} from "react";
 
 function SearchMovies() {
 
+  //states- input query, movies
   const [query, setQuery] = useState('');
+  //create the state for movies, and update that state appropriate
   const [movies, setMovies] = useState([]);
 
   const searchMovies = async (e) => {
     e.preventDefault();
 
-    const url = `https://api.themoviedb.org/3/movie/550?api_key=fa2fa335b93284c5277907e139256806&language=en-US&query=${query}&page=1&include_adult=false`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
 
-    try{
+    try {
       const res = await fetch(url);
       const data = await res.json();
-       setMovies(data.results);
-    }catch(err){
+      setMovies(data.results);
+    } catch (err) {
       console.error(err);
     }
-
-  };
-
+  }
 
   return (
-    <form className="form" onSubmit={searchMovies}>
-      <label className="label" htmlFor="query">
-        Movie Name
-      </label>
-      <input
-        className="input"
-        type="text"
-        name="query"
-        placeholder="i.e. Jurassic Park"
-        value={query}
-        onChange={(e)=>setQuery(e.target.value)}
-      />
-      <button className="button" type="submit">
-        Search
-      </button>
-      <div className="card-lists">
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">Movie Name</label>
+        <input className="input" type="text" name="query"
+          placeholder="i.e. Jurassic Park"
+          value={query} onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button" type="submit">Search</button>
+      </form>
+      <div className="card-list">
         {movies.map(movie => movie.title)}
       </div>
-    </form>
+    </>
   );
 }
 export default SearchMovies;
